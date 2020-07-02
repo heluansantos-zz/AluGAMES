4f5d82c0fa8d6abc49984e69121e8c700db7a6db
heluansantos <heluan.caetano@gmail.com>
2020-07-02 11:46:49 -0300

    Projeto AluGAMES v1.0 Estrutura do codigo

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Login() {
+  return (
+    <div className="App">
+      <text>Login Page</text>
+    </div>
+  );
+}
+
+export default Login;
