dc710defa52c7f27d7fe77c813314af5e70208fb
Renan <renan.nascimento.santana@hotmail.com>
2020-07-09 18:09:26 -0300

    estilizacao-menu+animacao

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -18,18 +18,24 @@ function Menu() {
   }
   return (
     <div className="App">
-      <p className="Title">Menu</p>
-      <button className="ButtonMenu" onClick={ListarJogos}>
-        Listar Jogos
-      </button>
-      <button className="ButtonMenu" onClick={CadastrarJogo}>
-        Cadastrar Jogo
-      </button>
-      <button className="ButtonMenu" onClick={Perfil}>
-        Perfil
-      </button>
-      <button className="ButtonMenu" onClick={Sair}>
-        Sair
+      <div id="flex-menu">
+        <div className="l-e-sbv">
+        <img src="/assets/logo-um.png" alt="Imagem de logo"/>
+        <p id="sbv">Seja bem vindo! <span>USUÁRIO</span></p>
+        </div>
+        <button id="imagem-perfil">Aqui perfil</button>
+      </div>
+      <button className="ButtonMenu" onClick={Perfil}><img className="imgs-opt" src="https://img.icons8.com/metro/40/000000/user-male.png"/>
+        <p className="btmt">MEU PERFIL</p>
+      </button><br></br>
+      <button className="ButtonMenu" onClick={CadastrarJogo}><img className="imgs-opt" src="https://img.icons8.com/metro/40/000000/address-book.png"/>
+      <p className="btmt">CADASTRAR JOGOS</p>
+      </button><br></br>
+      <button className="ButtonMenu" onClick={ListarJogos}><img className="imgs-opt" src="https://img.icons8.com/ios-filled/40/000000/design.png"/>
+      <p className="btmt">SOLICITAR EMPRÉSTIMOS</p>
+      </button><br></br>
+      <button className="exit" onClick={Sair}><img id="exit-menu" src="https://img.icons8.com/small/21/000000/exit.png"/>
+        SAIR
       </button>
     </div>
   );
